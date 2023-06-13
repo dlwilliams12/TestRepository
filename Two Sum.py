dc49b1bcc70e73f96cda454a04960fdc06b35d83
dlwilliams12 <devontaewilliams@me.com>
2023-06-13 11:06:49 -0700

    UPDATE* implement sum algorithm

--- Two Sum.py
+++ Two Sum.py
@@ -2,3 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(nums) - 1
+        sume = 0
+        keep_going = True
+        while keep_going:
+            sume = nums[i] + nums[j]
+            if sume == target:
+                return[i,j]
+        
