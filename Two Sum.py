323b475b93a663083bc2b004173d4dd4482c9038
dlwilliams12 <devontaewilliams@me.com>
2023-06-15 09:41:27 -0700

    Update* improved Solution class and twoSum method

--- Two Sum.py
+++ Two Sum.py
@@ -1,13 +1,17 @@
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(nums) - 1
-        sume = 0
-        keep_going = True
-        while keep_going:
-            sume = nums[i] + nums[j]
-            if sume == target:
-                return[i,j]
-        
+    def __init__(self, nums: List[int], target: int):
+        self.my_list = nums
+        self.my_target = target
+    
+    def twoSum(self) -> List[int]:
+        n = len(self.my_list)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if self.my_list[i] + self.my_list[j] == self.my_target:
+                    return [i, j]
+        return []  # No solution found
+    
+summe = Solution([2,7,11,15], 9)
+print(summe.twoSum())
