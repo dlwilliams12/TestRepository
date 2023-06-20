29f572e4b7be7076ec05598e07766ee482a256d8
dlwilliams12 <devontaewilliams@me.com>
2023-06-20 14:50:26 -0700

    Update* min_price initialization

--- Best Time to Buy and Sell Stock.py
+++ Best Time to Buy and Sell Stock.py
@@ -2,14 +2,16 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = 0
+        min_price = float("inf")
         max_profit = 0
         
         for price in prices:   
-            min_price = min(min_price, prices)
-            max_profit = max(max_profit, prices -  min_price)
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price -  min_price)
             
-            return max_profit
+        return max_profit
         
         
-        
+sol = Solution()
+prices = [7,1,5,3,6,4]
+print(sol.maxProfit(prices))
