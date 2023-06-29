f1805d53c0cbf5ef55b4129748cfb8d685c3b408
dlwilliams12 <devontaewilliams@me.com>
2023-06-29 14:26:54 -0700

    UPDATE* jump() modified

--- JumpGameII.py
+++ JumpGameII.py
@@ -2,18 +2,18 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        max_reach, n = 0, len(nums)
-        count = 1
-        for i, jump in enumerate(nums):
-            max_reach = max(max_reach, i + jump)
-            if max_reach <= i:
-                count -= 1
-                return count
-            if max_reach == 0:
-                max_reach = max(max_reach, i + jump - 1)
-            
-            if max_reach >= n -1:
-                return count
-            
-            count += 1
+        n = len(nums)
+        if n < 2:
+            return 0
+        max_reach, steps, count = nums[0], nums[0], 1
+        for i in range(1, n):
+            if i > steps:
+                count += 1
+                steps = max_reach
+            max_reach = max(max_reach, i + nums[i])
+        return count    
                 
+solution = Solution()
+nums = [1,2,1,1,1]
+result = solution.jump(nums)
+print(result)
