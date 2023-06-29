af48e7ee3fd6a2047b46ddf3f8f1eaf34209b7f2
dlwilliams12 <devontaewilliams@me.com>
2023-06-29 13:59:45 -0700

    UPDATE* jump algorithm

--- JumpGameII.py
+++ JumpGameII.py
@@ -2,4 +2,18 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
+        max_reach, n = 0, len(nums)
+        count = 1
+        for i, jump in enumerate(nums):
+            max_reach = max(max_reach, i + jump)
+            if max_reach <= i:
+                count -= 1
+                return count
+            if max_reach == 0:
+                max_reach = max(max_reach, i + jump - 1)
+            
+            if max_reach >= n -1:
+                return count
+            
+            count += 1
+                
