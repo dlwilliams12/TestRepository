2b76e3048483ce2cae1c3cf43d9b042eb1e0aba7
dlwilliams12 <devontaewilliams@me.com>
2023-07-17 11:41:43 -0700

    update* product method fully defined

--- product_of_array.py
+++ product_of_array.py
@@ -1,6 +1,14 @@
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        new_list = []
-        for i , self in enumerate(nums):
-            
+        res = [1] * (len(nums))
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix*= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
