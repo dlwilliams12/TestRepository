650c5fdeba669b141af2d484fdd8684677de3d7b
dlwilliams12 <devontaewilliams@me.com>
2023-07-17 11:23:28 -0700

    Initial commit

---
+++ product_of_array.py
@@ -0,0 +1,6 @@
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        new_list = []
+        for i , self in enumerate(nums):
+            
