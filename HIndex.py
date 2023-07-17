126e4fba1649727a99ceba07436b77263cc476b8
dlwilliams12 <devontaewilliams@me.com>
2023-07-17 11:24:14 -0700

    update* added product function

--- HIndex.py
+++ HIndex.py
@@ -2,4 +2,20 @@ from typing import List
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        new_list = []
+        current_max = 0
         
+        for i, cite in enumerate(citations):
+            if n-1 == 0:
+                return cite
+            if cite / n <= cite:
+                new_list.insert(i, cite)
+                current_max = max(new_list)
+                n -= i
+        return current_max
+            
+solution = Solution()
+nums = [3,0,6,1,5]
+print(solution.hIndex(nums))
